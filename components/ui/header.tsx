0985cbc576ce3c5d79fb4303c110184258b9b2e4
Trynax <kandoso111@gmail.com>
2025-12-19 07:43:11 +0100

    initial commit

---
+++ components/ui/header.tsx
@@ -0,0 +1,23 @@
+
+
+export function Header() {
+
+
+    return (
+        <header>
+            <div>
+                LOGO
+            </div>
+
+                
+            <div>
+                <a href="#">Explore Projects</a>
+                <a href="#">Explore Voting</a>
+                <a href="#">How it works</a>
+                <a href="#">About</a>
+            </div>
+
+            
+        </header>
+    )
+}
